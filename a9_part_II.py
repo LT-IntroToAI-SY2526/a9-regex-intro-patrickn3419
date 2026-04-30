cdaa0bd9e76e80b33634d1e6c24b0bf67bbbf39b
Patrick Nyman <pnyman@cps.edu>
2026-04-30 11:30:05 -0500

    attempted get_birth_date

--- a9_part_II.py
+++ a9_part_II.py
@@ -21,7 +21,7 @@ def get_planet_radius(planet_name: str) -> str:
     infobox_text = clean_text(get_first_infobox_text(get_page_html(planet_name)))
     # print(infobox_text)
     # TODO: fill this in
-    pattern = "Polar radius(?P<radius>[\d.\n]+)"
+    pattern = "Polar radius\n*(?P<radius>[\d.]+)"
     error_text = "Page infobox has no polar radius information"
     match = get_match(infobox_text, pattern, error_text)
     return match.group("radius")
@@ -39,7 +39,7 @@ def get_birth_date(name: str) -> str:
     infobox_text = clean_text(get_first_infobox_text(get_page_html(name)))
     print(infobox_text)
     # TODO: fill this in
-    pattern = "REPLACE ME"
+    pattern = "Born[\s\S]*?\((?P<birth>\d{4}-\d{2}-\d{2})\)"
     error_text = (
         "Page infobox has no birth information (at least none in xxxx-xx-xx format)"
     )
@@ -59,12 +59,12 @@ if __name__ == "__main__":
     print(f'Saturn has a polar radius of {get_planet_radius("Saturn")}km')
 
     # uncomment below lines for tests once you think you're getting the right output
-    # print('\n<<<< Running asserts, this might take a sec >>>>')
-    # assert get_planet_radius("Mars") == "3376.2", "Incorrect radius for Mars"
-    # assert get_planet_radius("Earth") == "6356.752", "Incorrect radius for Earth"
-    # assert get_planet_radius("Jupiter") == "66842", "Incorrect radius for Jupiter"
-    # assert get_planet_radius("Saturn") == "54364", "Incorrect radius for Saturn"
-    # print('\n<<<< Planet radius tests passed >>>>')
+    print('\n<<<< Running asserts, this might take a sec >>>>')
+    assert get_planet_radius("Mars") == "3376.2", "Incorrect radius for Mars"
+    assert get_planet_radius("Earth") == "6356.752", "Incorrect radius for Earth"
+    assert get_planet_radius("Jupiter") == "66842", "Incorrect radius for Jupiter"
+    assert get_planet_radius("Saturn") == "54364", "Incorrect radius for Saturn"
+    print('\n<<<< Planet radius tests passed >>>>')
 
     print("\n<<<<<<<<<<<<<< Testing Birth Dates >>>>>>>>>>>>>>")
     # should be 1906-12-09
@@ -76,7 +76,7 @@ if __name__ == "__main__":
     # should be 1949-01-17
     print(format_birth(get_birth_date("Anita Borg"), "Anita Borg"))
 
-    # uncomment below lines for tests once you think you're getting the right output
+    # uncomment below lines for tests once you think you're getting the right outbput
     # print('\n<<<< Running asserts, this might take a sec >>>>')
     # assert get_birth_date("Grace Hopper") == "1906-12-09", "Incorrect birth date for Grace Hopper"
     # assert get_birth_date("Alan Turing") == "1912-06-23", "Incorrect birth date for Alan Turing"
